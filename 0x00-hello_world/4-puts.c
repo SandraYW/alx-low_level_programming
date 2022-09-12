bd68507fbbd507b1dc2280bad95fea967d9a2e09
SandraYW <sandrawampah98@example.com>
2022-09-11 17:41:50 -0700

    Task 4 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
- * * main - Printing
- * *
- * * Return: Always 0 (Successful)
- * */
+ * main - Entry point
+ * 
+ * Return: Always 0 (Successful)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual
