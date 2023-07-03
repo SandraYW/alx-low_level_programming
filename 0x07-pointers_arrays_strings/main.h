2537b6115d5832af6cae39be47739ed605bc1e0c
SandraYW <86494383+SandraYW@users.noreply.github.com>
2023-07-03 20:22:44 +0000

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,24 +1,31 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
 
-char *_memset(char *s, char b, unsigned int n);
+/* Task 0: memset */
+void *_memset(char *s, char c, unsigned int n);
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+/* Task 1: memcpy */
+char *_memcpy(char *dest, const char *src, unsigned int n);
 
+/* Task 2: strchr */
 char *_strchr(char *s, char c);
 
+/* Task 3: strspn */
 unsigned int _strspn(char *s, char *accept);
 
+/* Task 4: strpbrk */
 char *_strpbrk(char *s, char *accept);
 
+/* Task 5: strstr */
 char *_strstr(char *haystack, char *needle);
 
+/* Task 7: print_chessboard */
 void print_chessboard(char (*a)[8]);
 
+/* Task 8: print_diagsums */
 void print_diagsums(int *a, int size);
 
-void set_string(char **s, char *to);
-
 #endif /* MAIN_H */
+
